475bc9aefa868d84a4a4eef4679fc533ff62e5e8
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-06-12 17:45:50 -0400

    Se agregaron comentarios al script

--- js/main.js
+++ js/main.js
@@ -1,29 +1,13 @@
+// Proyecto Final tiendita de Rocket league de Gabriel Lagrotteria para la clase de Javascript CoderHouse
+
 // Donde se veran los productos
 let contenedorProductos = document.getElementById('productos');
 
-// Llamo el local storage del autos.js
+// Llamo al row en el div de productos
 
-let productoAutos = JSON.stringify(localStorage.getItem('autos'));
+let productos = document.getElementById('productos').querySelector('.row');
 
-let productos = document.getElementById('productos');
-
-//Traigo el Json local
-
-fetch("../json/productos.json")
-    .then((response) => response.json())
-    .then((data) => {
-        data.forEach((item) => {
-            const li = document.createElement("li");
-            li.innerHTML = `
-        <h2>${item.id}</h2>
-         <p>${item.nombre}</p>
-         <b>${item.nombreCategoria}</b>
-       `;
-
-            productos.append(li);
-        });
-    });
-
+productosGeneral = "";
 
 //Llamo botones del menu
 
@@ -31,29 +15,33 @@ let botonTodos = document.getElementById('todos');
 let botonAutos = document.getElementById('traerAutos');
 let botonRuedas = document.getElementById('ruedas');
 
+// Llamo al H1 en el index aka titulo de Pagina
+
 let tituloPagina = document.getElementById('tituloSeccion');
 
-function categoriaAutos() {
-
-    // cambia el titulo de la pagina
-
-    tituloPagina.innerHTML = "Autos Rocket League";
-
-    let productos = document.getElementById('productos');
-
-    let productosCodigo = "";
-
-    //Cargo productos
 
 
+    // Defino variables para Paginacion
 
-    fetch("../json/productos.json")
+    let paginaInicial = 1;
+    let productosPagina = 20;
+
+//Traigo el Json local
+
+fetch("../json/productos.json")
         .then((response) => response.json())
         .then((data) => {
 
+        // le digo que traiga 20 por pagina
+           const paginaCategoria = (paginaInicial - 1) * productosPagina;
+           const paginaFin = paginaCategoria + productosPagina;
 
-            data.forEach((item) => {
-                if (item.categoria === 0) {
+           // Filtrar los productos por categoría y paginar
+           const productosFiltrados = data.filter(item).slice(paginaCategoria, paginaFin);
+
+            productosFiltrados.forEach((item) => {            
+
+                    tituloPagina.innerHTML = "inicio";
 
                     productosCodigo += `
                     <div class="col-lg-4">
@@ -62,17 +50,48 @@ function categoriaAutos() {
                     </div>
                     `;
 
-                }
                 productos.innerHTML = productosCodigo;
             })
         });
 
 
+// Funcion que trae solo los autos del json
 
+function categoriaAutos() {
 
+    // cambia el titulo de la pagina
 
+    let productosCodigo = "";
 
+    //Cargo productos con Json
+
+    fetch("../json/productos.json")
+        .then((response) => response.json())
+        .then((data) => {
+
+           const paginaCategoria = (paginaInicial - 1) * productosPagina;
+           const paginaFin = paginaCategoria + productosPagina;
+
+           // Filtrar los productos por categoría y paginar
+           const productosFiltrados = data.filter((item) => item.categoria === 0).slice(paginaCategoria, paginaFin);
+
+            productosFiltrados.forEach((item) => {            
+
+                    tituloPagina.innerHTML = "Autos Rocket League";
+
+                    productosCodigo += `
+                    <div class="col-lg-4">
+                    <p>${item.nombre}</p>
+                    <b>${item.nombreCategoria}</b>
+                    </div>
+                    `;
+
+                productos.innerHTML = productosCodigo;
+            })
+        });
 
 }
+
+// le asigno la funcion al boton del menu al hacer click
 
 botonAutos.addEventListener("click", categoriaAutos);
